ec78fb0bca800070762486acd474871a8df17b1d
Jonathan Pynes <jonpynes@gmail.com>
2021-10-25 15:52:13 -0500

    farleft bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 //design should look like this https://bashooka.com/wp-content/uploads/2018/11/to-do-app-ui-design-5.jpg
 import TodoList from "./components/TodoList";
-
+import LogoIcon from "./icons/logo.svg";
+import InboxIcon from "./icons/inbox.svg";
+import StarIcon from "./icons/star_border.svg";
+import CalendarIcon from "./icons/calendar_today.svg";
+import CheckedIcon from "./icons/check.svg";
+import TrashIcon from "./icons/delete_outline.svg";
+import ProfileImg from "./img/profile.jpg";
+import SpreadIcon from "./icons/spread.svg";
 import styled from "styled-components";
 
 function App() {
@@ -9,14 +16,35 @@ function App() {
             <BodyCSS>
                 <MainDivCSS>
                     <FarLeftBarCSS>
-                        <TopULFarLeftBarCSS>
-                            <button>Home</button>
-                            <button>Inbox</button>
-                            <button>Star</button>
-                            <button>Calendar</button>
-                            <button>Check</button>
-                            <button>Trash</button>
-                        </TopULFarLeftBarCSS>
+                        <IconsFarLeftBarCSS>
+                            <IconButton onClick={console.log("clicked")}>
+                                <img src={LogoIcon} alt="home" />
+                            </IconButton>
+                            <IconButton>
+                                <img src={InboxIcon} alt="inbox" />
+                            </IconButton>
+                            <IconButton>
+                                <img src={StarIcon} alt="star" />
+                            </IconButton>
+                            <IconButton>
+                                <img src={CalendarIcon} alt="calendar" />
+                            </IconButton>
+                            <IconButton>
+                                <img src={CheckedIcon} alt="check" />
+                            </IconButton>
+                            <IconButton>
+                                <img src={TrashIcon} alt="delete" />
+                            </IconButton>
+
+                            {/* Probably a better way to do the last bit below, would love a tip for the css below... I thought padding-top: auto would work but no? */}
+
+                            <ProfileButton>
+                                <ProfileImgCSS src={ProfileImg} alt="profile" />
+                            </ProfileButton>
+                            <SpreadIconButton>
+                                <img src={SpreadIcon} alt="menu" />
+                            </SpreadIconButton>
+                        </IconsFarLeftBarCSS>
                     </FarLeftBarCSS>
                     <LeftBarCSS></LeftBarCSS>
                     <RightBoxCSS></RightBoxCSS>
@@ -58,7 +86,36 @@ const FarLeftBarCSS = styled.div`
     flex-direction: column;
 `;
 
-const TopULFarLeftBarCSS = styled.ul``;
+const IconsFarLeftBarCSS = styled.ul`
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    padding: 0;
+`;
+
+const IconButton = styled.button`
+    padding: 3rem 0 0 0;
+    border: none;
+    background: none;
+`;
+
+const ProfileButton = styled.button`
+    padding-top: 15rem;
+    border: none;
+    background: none;
+`;
+const ProfileImgCSS = styled.img`
+    height: 2.5rem;
+    width: 2.5rem;
+    object-fit: cover;
+    border-radius: 50%;
+`;
+
+const SpreadIconButton = styled.button`
+    padding: 1rem 0 0 0;
+    border: none;
+    background: none;
+`;
 
 const LeftBarCSS = styled.div`
     position: absolute;
